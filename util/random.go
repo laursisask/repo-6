e425abfb14cfc6f2fe60fca1f73ece5bc376380a
Dragos Ciupureanu <dciupureanu@adaptavist.com>
2021-10-13 16:09:37 +0100

    fix(random): use UUID v4

--- util/random.go
+++ util/random.go
@@ -1,9 +1,9 @@
 package util
 
 import (
+	"github.com/google/uuid"
 	"math/rand"
 	"time"
-	"unsafe"
 )
 
 const (
@@ -25,29 +25,7 @@ func NewRandomStringGenerator(stringSize int) *RandomStringGenerator {
 	}
 }
 
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const (
-	letterIdxBits = 6                    // 6 bits to represent a letter index
-	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
-)
-
 func (gen *RandomStringGenerator) RandomString() string {
-	var src = gen.seededRandom
-	var n = gen.Size
-	b := gen.buffer
-	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
-	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = src.Int63(), letterIdxMax
-		}
-		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-			b[i] = letterBytes[idx]
-			i--
-		}
-		cache >>= letterIdxBits
-		remain--
-	}
-
-	return *(*string)(unsafe.Pointer(&b))
+	var random, _ = uuid.NewRandom()
+	return random.String()
 }
